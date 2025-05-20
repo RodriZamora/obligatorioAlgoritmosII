1e81e02a92ce76d4e1f7a8ff3af86e195fe1b3f5
Rodrigo Zamora <zamorarodrigo96@gmail.com>
2025-05-20 11:56:25 -0300

    Se agregan test de pruebas en buscarViajero

--- Obl-AED2-mar25/src/test/java/sistema/Test03BuscarViajero.java
+++ Obl-AED2-mar25/src/test/java/sistema/Test03BuscarViajero.java
@@ -21,13 +21,62 @@ public class Test03BuscarViajero {
     @Test
     void buscarViajeroOk() {
         s.registrarViajero("1.914.689-5", "Guillermo", "guille@ort.edu.uy", 35, Categoria.ESTANDAR);
-        s.registrarViajero("1.914.689-6", "Guillermo", "guille@ort.edu.uy", 35, Categoria.PLATINO);
         s.registrarViajero("1.913.689-5", "MaxVerstappen", "verstappen@ort.edu.uy", 28, Categoria.PLATINO);
         s.registrarViajero("1.919.689-5", "CharlesLeclerc", "leclerc@ort.edu.uy", 27, Categoria.FRECUENTE);
         retorno = s.buscarViajeroPorCedula("1.914.689-5");
         assertEquals(Retorno.Resultado.OK, retorno.getResultado());
         assertEquals(1, retorno.getValorInteger());
         assertEquals("1.914.689-5;Guillermo;guille@ort.edu.uy;35;ESTANDAR", retorno.getValorString());
+    }
+
+    @Test
+    void buscarViajeroError1() {
+        s.registrarViajero("1.914.689-5", "Guillermo", "guille@ort.edu.uy", 35, Categoria.ESTANDAR);
+        s.registrarViajero("1.913.689-5", "MaxVerstappen", "verstappen@ort.edu.uy", 28, Categoria.PLATINO);
+        s.registrarViajero("1.919.689-5", "CharlesLeclerc", "leclerc@ort.edu.uy", 27, Categoria.FRECUENTE);
+        retorno = s.buscarViajeroPorCedula("");
+        assertEquals(Retorno.Resultado.ERROR_1, retorno.getResultado());
+        assertEquals(0, retorno.getValorInteger());
+        assertEquals("La cedula no puede ser nula o vacia", retorno.getValorString());
+
+        retorno = s.buscarViajeroPorCedula(null);
+        assertEquals(Retorno.Resultado.ERROR_1, retorno.getResultado());
+        assertEquals(0, retorno.getValorInteger());
+        assertEquals("La cedula no puede ser nula o vacia", retorno.getValorString());
+    }
+
+    @Test
+    void buscarViajeroError2() {
+        s.registrarViajero("1.914.689-5", "Guillermo", "guille@ort.edu.uy", 35, Categoria.ESTANDAR);
+        s.registrarViajero("1.913.689-5", "MaxVerstappen", "verstappen@ort.edu.uy", 28, Categoria.PLATINO);
+        s.registrarViajero("1.919.689-5", "CharlesLeclerc", "leclerc@ort.edu.uy", 27, Categoria.FRECUENTE);
+        retorno = s.buscarViajeroPorCedula("1.91.4.689-5");
+        assertEquals(Retorno.Resultado.ERROR_2, retorno.getResultado());
+        assertEquals(0, retorno.getValorInteger());
+        assertEquals("El formato de la cedula no es valido", retorno.getValorString());
+
+        retorno = s.buscarViajeroPorCedula("1.914.6895");
+        assertEquals(Retorno.Resultado.ERROR_2, retorno.getResultado());
+        assertEquals(0, retorno.getValorInteger());
+        assertEquals("El formato de la cedula no es valido", retorno.getValorString());
+
+        retorno = s.buscarViajeroPorCedula("1914.689-5");
+        assertEquals(Retorno.Resultado.ERROR_2, retorno.getResultado());
+        assertEquals(0, retorno.getValorInteger());
+        assertEquals("El formato de la cedula no es valido", retorno.getValorString());
+    }
+
+    @Test
+    void buscarViajeroError3() {
+        s.registrarViajero("1.914.689-5", "Guillermo", "guille@ort.edu.uy", 35, Categoria.ESTANDAR);
+        s.registrarViajero("1.913.689-5", "MaxVerstappen", "verstappen@ort.edu.uy", 28, Categoria.PLATINO);
+        s.registrarViajero("1.919.689-5", "CharlesLeclerc", "leclerc@ort.edu.uy", 27, Categoria.FRECUENTE);
+
+        retorno = s.buscarViajeroPorCedula("4.745.897-9");
+        assertEquals(Retorno.Resultado.ERROR_3, retorno.getResultado());
+        assertEquals(0, retorno.getValorInteger());
+        assertEquals("No existe un viajero registrado con esa cedula", retorno.getValorString());
+
     }
 
 }
